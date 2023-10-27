79b8db04ed160163db4e3ff1d4873d3fbace9f8e
KelvinMvungi <kelvin.mvungi01@gmail.com>
2023-10-27 17:14:39 +0300

    Add the playround function for the console

--- index.js
+++ index.js
@@ -24,12 +24,14 @@ let playerScore = 0;
     }
     
     let selected;
+    let playerSelection;
+    let computerSelection;
 
     rockButton.addEventListener('click', () => {
         selected = 'rock';
         const playerSelection = selected[0].toUpperCase() + selected.slice(1);
         playerChoiceSpan.innerText = `You selected: ${playerSelection} \n`;
-        choiceOfComputer();
+        computerSelection = choiceOfComputer();
         playRound(playerSelection, computerSelection)
       });
 
@@ -37,45 +39,43 @@ let playerScore = 0;
         selected = 'paper';
         const playerSelection = selected[0].toUpperCase() + selected.slice(1);
         playerChoiceSpan.innerText = `You selected: ${playerSelection} \n`;
-        choiceOfComputer();
+        computerSelection = choiceOfComputer();
         playRound(playerSelection, computerSelection)
-        
       });
       
       scissorsButton.addEventListener('click', () => {
         selected = 'scissors';
         const playerSelection = selected[0].toUpperCase() + selected.slice(1);
         playerChoiceSpan.innerText = `You selected: ${playerSelection} \n`;
-        choiceOfComputer();
+        computerSelection = choiceOfComputer();
         playRound(playerSelection, computerSelection);
     });
 
-    // function playRound(playerSelection, computerSelection) {
-    //     if (computerSelection == 'Rock' && playerSelection == 'Scissors') {
-    //         console.log("You lost this round, Rock beats Scissors!");
-    //         computerScore++;
-    //     } else if (computerSelection == 'Rock' && playerSelection == 'Paper') {
-    //         console.log("You won this round, Paper beats Rock!");
-    //         playerScore++;
-    //     } else if (computerSelection == 'Scissors' && playerSelection == 'Paper') {
-    //         console.log("You lost this round, Scissors beats Paper!");
-    //         computerScore++;
-    //     } else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
-    //         console.log("You won this round, Rock beats Scissors!");
-    //         playerScore++;
-    //     } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
-    //         console.log("You lost this round, Paper beats Rock!");
-    //         computerScore++;
-    //     } else if (computerSelection == 'Paper' && playerSelection == 'Scissors') {
-    //         console.log("You won this round, Scissors beats Rock!");
-    //         playerScore++;
-    //     } else if (computerSelection == playerSelection) {
-    //         console.log("It's a tie!")
-    //     } else {
-    //         return;
-    //     }
-    // }
-    // playRound(playerSelection, computerSelection);
+    function playRound(playerSelection, computerSelection) {
+        if (computerSelection == 'Rock' && playerSelection == 'Scissors') {
+            console.log("You lost this round, Rock beats Scissors!");
+            computerScore++;
+        } else if (computerSelection == 'Rock' && playerSelection == 'Paper') {
+            console.log("You won this round, Paper beats Rock!");
+            playerScore++;
+        } else if (computerSelection == 'Scissors' && playerSelection == 'Paper') {
+            console.log("You lost this round, Scissors beats Paper!");
+            computerScore++;
+        } else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
+            console.log("You won this round, Rock beats Scissors!");
+            playerScore++;
+        } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
+            console.log("You lost this round, Paper beats Rock!");
+            computerScore++;
+        } else if (computerSelection == 'Paper' && playerSelection == 'Scissors') {
+            console.log("You won this round, Scissors beats Rock!");
+            playerScore++;
+        } else if (computerSelection == playerSelection) {
+            console.log("It's a tie!")
+        } else {
+            return;
+        }
+    }
 
 
 
